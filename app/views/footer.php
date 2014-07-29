a5a15d176f1b7a7c71151abe88ad654d89c8df93
Mario <mmontesgonz@gmail.com>
2014-07-29 20:49:12 +0200

    views

---
+++ app/views/footer.php
@@ -0,0 +1,14 @@
+</div> <!-- #main-container -->
+
+<div class="footer-container">
+            <footer class="wrapper">
+                <h3>footer</h3>
+            </footer>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ app/views/inicio.php
@@ -0,0 +1,14 @@
+<ul>
+	<li>
+		<h2>Anónimos</h2>
+		<p>Tus datos blablabla...</p>
+	</li>
+
+	<li>
+		<h2>Rápido</h2>
+		<p>Tu delegado lo ve al instante</p>
+	</li>
+
+</ul>
+
+<a href="inicio/login">Entrar</a>
