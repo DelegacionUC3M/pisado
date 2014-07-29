89d283afa8a6bdca3b0b0d425d4d068356c8f31b
Mario <mmontesgonz@gmail.com>
2014-07-29 18:48:48 +0200

    Pisado model

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -1,13 +1,8 @@
 <?php
 
-class MainController {
+class Controller {
 	
 	function __construct() {
-		$controller = (isset($_GET['c']) && !empty($_GET['c'])) ? $_GET['c'].'Controller' : 'inicioController';
-		$action = (isset($_GET['a']) && !empty($_GET['a'])) ? $_GET['a'] : 'index';
-
-		$load = new $controller();
-		$load->$action();
 	}
 
 }
