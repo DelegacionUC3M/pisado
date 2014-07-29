475cf56b0e05acf3901d591fe658a7c7470f4692
Mario <mmontesgonz@gmail.com>
2014-07-29 17:28:03 +0200

    User

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 
+require_once('config.php');
+
 function __autoload($class) {
 
-	if(file_exists('app/controllers/'.$class.'.php')) {
+	if (file_exists('app/controllers/'.$class.'.php')) {
 		include ('app/controllers/'.$class.'.php');
-	}
-	else if (file_exists('app/models/'.$class.'.php')) {
+	} else if (file_exists('app/models/'.$class.'.php')) {
 		include ('app/models/'.$class.'.php');
-	}
-	else if (file_exists('app/views/'.$class.'.php')) {
+	} else if (file_exists('app/views/'.$class.'.php')) {
 		include ('app/views/'.$class.'.php');
-	}
+	} else if (file_exists('app/lib/'.$class.'.php')) {
+		include ('app/lib/'.$class.'.php');
+	} 
 
 	if(!class_exists($class)) {
 		errorController::index();
