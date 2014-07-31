5798a37fc768a7e15f98a41bc5dc1361145212df
Mario <mmontesgonz@gmail.com>
2014-07-31 13:04:34 +0200

    Cambios en session

--- app/views/login.php
+++ app/views/login.php
@@ -2,5 +2,7 @@
 	<input type="text" name="nia" placeholder="Nia" />
 	<input type="password" name="password" placeholder="Contraseña" />
 
+	<p><?php echo isset($error) ? $error : '' ?></p>
+
 	<input type="submit" />
 </form>
