6e7c2805a1b0e6b2b5f38e3ff190ce03b3b3faf2
Mario <mmontesgonz@gmail.com>
2014-09-12 00:49:55 +0200

    CSS

--- app/views/mailCreateU.php
+++ app/views/mailCreateU.php
@@ -6,9 +6,9 @@
         <link rel="stylesheet" href="/pisado/assets/css/main.css">
     </head>
 	<body>
-		<p class="first">Su solicitud de P.I.S.A.D.O. se ha realizado con &eacute;xito</p>
+		<p class="first">Su solicitud de PISADO se ha realizado con &eacute;xito</p>
 
-		<p>Si desea ver su pisado de nuevo est&aacute; disponible desde la aplicacion de P.I.S.A.D.O. en el apartado "Mis pisados", o direcatemente desde el enlace: <a href="https://delegacion.uc3m.es/pisado/pisado/view/<?php echo $pisado->id ?>">P.I.S.A.D.O.</a><!-- No funciono :D-->
+		<p>Si desea ver su pisado de nuevo est&aacute; disponible desde la aplicacion de PISADO en el apartado "Mis pisados", o direcatemente desde el enlace: <a href="https://delegacion.uc3m.es/pisado/pisado/view/<?php echo $pisado->id ?>">PISADO</a>.<!-- No funciono :D-->
 		</p>
 		
 		<p>Este correo se ha generado automaticamente, por favor no lo respondas. Si deseas ponerte en contacto con 
