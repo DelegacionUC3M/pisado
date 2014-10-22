865c10be2b1502b54030a9288815f1cabdd21207
Drimmark <drimmark@Midgard.Yggdrasil>
2014-10-22 22:32:59 +0200

    Modificados los modelos en base al nuevo modelo de db. Tambien añadido define en config.php

--- app/models/DBDelegados.php
+++ app/models/DBDelegados.php
@@ -6,7 +6,7 @@ class DBDelegados {
 	 * Get nombre of titulacion
 	 */
 	public static function findByIdTitulacion($id) {
-	//	$db = new DB();
+	//	$db = new DB(SQL_DB_DELEGADOS);
 	//	$db->run();
 
 	//	if ($db->count() == 1) {
@@ -21,7 +21,7 @@ class DBDelegados {
 	 * Get id of titulacion
 	 */
 	public static function findByNameTitulacion($nombre) {
-	//	$db = new DB();
+	//	$db = new DB(SQL_DB_DELEGADOS);
 	//	$db->run();
 
 	//	if ($db->count() == 1) {
@@ -38,7 +38,7 @@ class DBDelegados {
 	 * @return  array with id_titulacion, rol and curso
 	 */
 	public static function findDelegado($nia) {
-	//	$db = new DB();
+	//	$db = new DB(SQL_DB_DELEGADOS);
 	//	$db->run();
 
 	//	if ($db->count() == 1) {
