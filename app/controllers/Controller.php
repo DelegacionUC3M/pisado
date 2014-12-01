b45fb81aba37cd948889b28e606ea2e66445b211
Drimmark <root@Midgard.Yggdrasil>
2014-12-01 10:08:23 +0100

    Corregidas cabeceras

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -74,8 +74,9 @@ class Controller {
 	      	'Reply-To: delegest@gmail.com' . "\r\n";
 	    $cabeceras .= 'BCC: ';
 	    foreach ($destinatarios as $destinatario) {
-	    	$cabeceras .= $destinatario['nia'] . "@alumnos.uc3m.es" . "," . "\r\n";
+	    	$cabeceras .= $destinatario['nia'] . "@alumnos.uc3m.es" . "\r\n";
 	    }
+	    $cabeceras .= "\r\n"
 
 		mail(NULL , $titulo, $cuerpo, $cabeceras);
 	}
