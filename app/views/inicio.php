474d4a1e44ccebdb1272dddaacfe307f73d09d2c
drimmark <axel.bc099@gmail.com>
2015-04-14 10:12:00 +0200

    texto añadido

--- app/views/inicio.php
+++ app/views/inicio.php
@@ -2,14 +2,20 @@
 	<div class="wrapper">
 
 		<h2>¿Qué es un PISADO?</h2>
-		<p>PISADO es Parte Informativo sobre la Actividad Docente</p>
+		<p>¿Sabes qué métodos principales tienes para quejarte de un profesor por su método docente?</p>
+		<p>Una vía es la queja formal, que se hace por el Registro por solicitud genérica. Eso sí, el profesor sabrá en el momento de leer la queja, el nombre, NIA, DNI grado en curso, número de teléfono e incluso dónde vives.</p>
+		<p>¿Qué otra forma te proporciona la Delegación? El P.I.S.A.DO (Parte Informativo Sobre la Actividad Docente).</p>
 		<ul>
 			<li>
 				<h2>Anónimo</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut massa non enim pharetra lacinia eu ac neque. Curabitur sollicitudin tincidunt magna non euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam tincidunt orci sed diam auctor mollis. Morbi quis bibendum risus, laoreet convallis nisl.</p>
+				<p>El PISADO es una queja hecha para que el anonimato del estudiante quede salvaguardado. El profesor únicamente sabrá el curso y el grado de la persona que hizo la queja.</p>
+				<p>Ni siquiera nosotros, los Delegados, sabremos quién eres a menos que tú lo permitas, pero eso no quita que podamos mantener contacto para que sepamos más acerca de tu queja.</p>
+				<p>Sí, te pedimos que inicies sesión con tu NIA, pero eso es simplemente para que el sistema esté seguro de que la persona que realiza la queja es un estudiante de la Universidad.</p>
 			</li><li>
 				<h2>Rápido</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut massa non enim pharetra lacinia eu ac neque. Curabitur sollicitudin tincidunt magna non euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam tincidunt orci sed diam auctor mollis. Morbi quis bibendum risus, laoreet convallis nisl.</p>
+				<p>Como el PISADO se hace por vía página web, la recibiremos inmediatamente, y con la misma celeridad nos pondremos a tramitar tu queja.</p>
+				<p>Dependiendo de cómo sea y cómo quieres que se tramite, lo podremos presentar cuanto antes tengamos todos los asuntos claros o bien lo podremos presentar en la llamada Comisión Académica de Grado.</p>
+				<p>La Comisión Académica es un órgano hecho para evaluar el transcurso del Grado y se convoca una reunión cada final de cuatrimestre. En él también se evalúa cómo ha ido la docencia de ese cuatrimestre en cuestión y allí podremos presentar todas las quejas anónimas.</p>
 			</li>
 		</ul>
 	</div>
