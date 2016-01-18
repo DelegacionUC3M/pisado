8d2fd1050d608833a4e3947d7f647572665d4b40
Drimmark <axel.bc099@gmail.com>
2016-01-18 14:02:14 +0100

    pgsql model

--- sql/pisado.sql
+++ sql/pisado.sql
@@ -1,105 +1,48 @@
--- phpMyAdmin SQL Dump
--- version 4.0.6deb1
--- http://www.phpmyadmin.net
---
--- Servidor: localhost
--- Tiempo de generación: 22-10-2014 a las 21:26:06
--- Versión del servidor: 5.5.37-0ubuntu0.13.10.1
--- Versión de PHP: 5.5.3-1ubuntu2.6
+CREATE TABLE comentario_group (
+  id serial PRIMARY KEY,
+  id_group bigint NOT NULL,
+  nia bigint NOT NULL,
+  nombre varchar(50) NOT NULL,
+  date date NOT NULL,
+  text text NOT NULL,
+  FOREIGN KEY (id_group) REFERENCES group (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE comentario_pisado (
+  id serial PRIMARY KEY,
+  id_pisado bigint NOT NULL,
+  nia bigint NOT NULL,
+  nombre varchar(50) NOT NULL,
+  date date NOT NULL,
+  text text NOT NULL,
+  FOREIGN KEY (id_pisado) REFERENCES pisado (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
+CREATE TABLE group (
+  id serial PRIMARY KEY,
+  subject varchar(50) NOT NULL,
+  date date NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+CREATE TABLE archive (
+  id serial PRIMARY KEY,
+  id_pisado bigint UNIQUE NOT NULL,
+  date date NOT NULL,
+  FOREIGN KEY (id_pisado) REFERENCES pisado (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
---
--- Base de datos: `pisado`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `comentario_group`
---
-
-CREATE TABLE IF NOT EXISTS `comentario_group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_group` int(11) NOT NULL,
-  `nia` int(9) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `text` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `comentario_pisado`
---
-
-CREATE TABLE IF NOT EXISTS `comentario_pisado` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pisado` int(11) NOT NULL,
-  `nia` int(9) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `text` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=15 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `group`
---
-
-CREATE TABLE IF NOT EXISTS `group` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `subject` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `archive`
---
-
-CREATE TABLE IF NOT EXISTS `archive` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_pisado` int(11) UNIQUE NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pisado`
---
-
-CREATE TABLE IF NOT EXISTS `pisado` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nia` int(9) NOT NULL,
-  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `autor` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  `id_titulacion` int(11) NOT NULL,
-  `asignatura` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `curso` int(1) NOT NULL,
-  `grupo` int(11) NOT NULL,
-  `profesor` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
-  `texto` text COLLATE utf8_unicode_ci NOT NULL,
-  `id_group` int(11) DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE pisado (
+  id serial PRIMARY KEY,
+  nia bigint NOT NULL,
+  email varchar(40) NOT NULL,
+  autor varchar(40) NOT NULL,
+  date date NOT NULL,
+  id_titulacion bigint NOT NULL,
+  asignatura varchar(50) NOT NULL,
+  curso smallint NOT NULL,
+  grupo bigint NOT NULL,
+  profesor varchar(40) NOT NULL,
+  texto text NOT NULL,
+  id_group bigint,
+  FOREIGN KEY (id_group) REFERENCES group (id)
+);
