8e7e0b8813c0f7790734b335f2f0858bab4574b5
Patataman <shas_arroyo@hotmail.com>
2016-01-26 14:55:19 +0100

    postgresql

--- app/models/DBDelegados.php
+++ app/models/DBDelegados.php
@@ -59,16 +59,24 @@ class DBDelegados {
 	 */
 	public static function findDelegado($nia) {
 		$db = new DB(SQL_DB_DELEGADOS);
-		$db->run('SELECT personas.id_titulacion, personas.curso, delegados.del_escuela, delegados.del_titulacion, delegados.del_curso, delegados.cargo
-					FROM delegados LEFT JOIN personas ON personas.id = delegados.id where personas.nia = ?;', array($nia));
+		$db->run('SELECT id, id_titulacion, curso FROM personas WHERE nia = ?;', array($nia));
 		$data = $db->data();
-
-		if($data[0]['del_escuela'] != 0) {
-			$rol = ROL_DELEGADO_CENTRO;
-		} else if($data[0]['del_titulacion'] != 0) {
+		//get delegadoCurso
+		$db->run('SELECT id FROM delegadosCurso WHERE id = ?;', array($data[0]['id']));
+		$delCurso = $db->data();
+		//get delegadoCentro
+		$db->run('SELECT id FROM delegadosCentro WHERE id = ?;', array($data[0]['id']));
+		$delCentro = $db->data();
+		//get delegadoTitulacion
+		$db->run('SELECT id FROM delegadosTitulacion WHERE id = ?;', array($data[0]['id']));
+		$delTitulacion = $db->data();
+		
+		if($delCentro[0] != null) {
+			$rol = ROL_DELEGADO_CENTRO;	
+		} else if($delTitulacion[0] != null) {
 			$rol = ROL_DELEGADO_TITULACION;
-		} else if($data[0]['del_curso'] != 0) {	//Faltan los casos especiales en que alguien que no es delegado ejerce como tal.
-			$rol = ROL_DELEGADO_CURSO;
+		} else if($delCurso[0] != null) {	//Faltan los casos especiales en que alguien que no es delegado ejerce como tal.
+			$rol = ROL_DELEGADO_CURSO
 		} else {
 			$rol = null;
 		}
@@ -88,7 +96,7 @@ class DBDelegados {
 	public static function findDelegadosCurso($id_titulacion,$curso) {
 		$db = new DB(SQL_DB_DELEGADOS);
 		$db->run('SELECT personas.nia, personas.nombre, personas.apellido1 
-				FROM delegados LEFT JOIN personas ON personas.id = delegados.id
+				FROM delegadosCurso LEFT JOIN personas ON personas.id = delegadosCurso.id
 				WHERE personas.id_titulacion = ? AND personas.curso = ?;', array($id_titulacion, $curso));
 		$data = $db->data();
 
@@ -98,8 +106,8 @@ class DBDelegados {
 	public static function findDelegadosTitulacion($id_titulacion) {
 		$db = new DB(SQL_DB_DELEGADOS);
 		$db->run('SELECT personas.nia, personas.nombre, personas.apellido1 
-				FROM delegados LEFT JOIN personas ON personas.id = delegados.id
-				WHERE personas.id_titulacion = ? AND delegados.del_titulacion = 1;', array($id_titulacion));
+				FROM delegadosTitulacion LEFT JOIN personas ON personas.id = delegadosTitulacion.id
+				WHERE personas.id_titulacion = ?;', array($id_titulacion));
 		$data = $db->data();
 
 		return isset($data) ? $data : null;
