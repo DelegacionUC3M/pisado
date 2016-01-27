d56a7f872bde040129f9b0796fbb36fd6f83d751
Patataman <shas_arroyo@hotmail.com>
2016-01-27 13:00:20 +0100

    debug

--- app/models/Group.php
+++ app/models/Group.php
@@ -138,7 +138,7 @@ class Group {
 	public function save() {
 		$db = new DB(SQL_DB_PISADO);
 		$query = $db->run('INSERT INTO "group" (subject,date) VALUES (?,NOW())', array($this->subject));
-		print_r('id1:'.$db->lastId());
+		var_dump('id1:'.$db->lastId());
 		if ($query) {
 			$this->id = $db->lastId();
 			print_r('id2'.$db->lastId());
