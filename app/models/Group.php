beb5cf51d0f3321f5fd7b8d079b940b46e5a9170
Patataman <shas_arroyo@hotmail.com>
2016-01-27 11:40:19 +0100

    debug

--- app/models/Group.php
+++ app/models/Group.php
@@ -139,6 +139,8 @@ class Group {
 		$db = new DB(SQL_DB_PISADO);
 		$query = $db->run('INSERT INTO "group" (subject,date) VALUES (?,NOW())', array($this->subject));
 		if ($query) {
+			var_dump('ola caracola');
+			die();
 			$this->id = $db->lastId();
 			return true;
 		} else {
