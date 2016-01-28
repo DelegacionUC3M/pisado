401df48769e99a760398145f70980a57a49a511b
Patataman <shas_arroyo@hotmail.com>
2016-01-28 13:54:56 +0100

    debug

--- app/models/Group.php
+++ app/models/Group.php
@@ -63,7 +63,7 @@ class Group {
 				$groups[] = $group;
 			}
     	}
-    	var_dump($groups);
+    	var_dump(isset($groups) && !empty($groups[0]->pisados));
 		if (isset($groups) && !empty($groups[0]->pisados)) { return $groups; }
     	else { return array(); }
 	}
@@ -85,7 +85,7 @@ class Group {
 				$groups[] = $group;
 			}
     	}
-    	var_dump($groups);
+    	var_dump(isset($groups) && !empty($groups[0]->pisados));
     	if (isset($groups) && !empty($groups[0]->pisados)) { return $groups; }
     	else { return array(); }
 		
@@ -126,7 +126,7 @@ class Group {
 				$groups[] = $group;
 			}
     	}
-    	var_dump($groups);
+    	var_dump(isset($groups) && !empty($groups[0]->pisados));
 		if (isset($groups) && !empty($groups[0]->pisados)) { return $groups; }
     	else { return array(); }
 	}
